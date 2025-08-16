a09421e906528be6675d310aa6c08e275ffa0081
muhammad sheik nauman <noumansheik07gmail.com>
2025-08-16 16:33:52 +0530

    Enhanced responsiveness

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -55,28 +55,31 @@ const Manager = () => {
         <>
             <div className="absolute inset-0 -z-10 h-screen w-full bg-green-100 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div></div>
            
-            <div className=" mx-auto bg-slate-450 md:mycontainer shadow-lg w-180 h-50">
+            <div className=" mx-auto bg-slate-450 md:mycontainer shadow-lg w-full max-w-2xl h-full max-h-2xl">
                 <div className='text-black flex flex-col p-4 justify-between'>
                     <h1 className='text-center'><span className='text-green-500 font-bold text-3xl '>&lt;PassOP&gt;</span></h1>
                     <p className='text-center'>your own password manager</p>
-                    <input className="bg-slate-100 border border-gray-300 rounded-full px-2 " value={form.site} onChange={handleChange} name='site' type="text" placeholder="enter website url " />
-                    <div className='flex flex-row space-x-5 mt-4'>
-                        <input className='bg-slate-100 border border-gray-300 rounded-full w-100 px-2 ' value={form.username} onChange={handleChange} name='username' type="text" placeholder='enter username ' />
-                        <div className="relative w-40">
+                    <input className="bg-slate-100 border border-gray-300 rounded-full px-2 w-full " value={form.site} onChange={handleChange} name='site' type="text" placeholder="enter website url " />
+                    <div className='flex flex-row sm:space-x-5 mt-4'>
+                        <input className='bg-slate-100 border border-gray-300 rounded-full w-full px-2 ' value={form.username} onChange={handleChange} name='username' type="text" placeholder='enter username ' />
+                        <div className="relative">
                             <input
                                 ref={passwordRef}
-                                className='bg-slate-100 border border-gray-300 rounded-full w-67 px-2 pr-8'
+                                className='bg-slate-100 border border-gray-300 rounded-full w-full px-2 pr-5'
                                 value={form.password}
                                 onChange={handleChange}
                                 name='password'
                                 type={showPwd ? "text" : "password"}
                                 placeholder='enter password'
                             />
-                            <span className='absolute inset-y-0 right-2 flex items-center cursor-pointer ' onClick={showPassword}>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-gray-500 hover:text-black">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12s3.75-6.75 9.75-6.75S21.75 12 21.75 12s-3.75 6.75-9.75 6.75S2.25 12 2.25 12z" />
-                                    <circle cx="12" cy="12" r="3" />
-                                </svg>
+                            <span className='px absolute inset-y-0 right-2 flex items-center cursor-pointer ' onClick={showPassword}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" 
+              viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" 
+              className="w-4 h-4  text-gray-500 hover:text-black">
+              <path strokeLinecap="round" strokeLinejoin="round" 
+                d="M2.25 12s3.75-6.75 9.75-6.75S21.75 12 21.75 12s-3.75 6.75-9.75 6.75S2.25 12 2.25 12z" />
+              <circle cx="12" cy="12" r="3" />
+            </svg>
                             </span>
                         </div>
                     </div>
